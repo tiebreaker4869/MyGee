430938bcd9cefebe27377280564bfb494a481c7f
SUNLIFAN <1656680420@qq.com>
2022-07-20 19:18:44 +0800

    day1 part finished

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"gee"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, "this is index page")
+	})
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, "this is hello page")
+	})
+
+	log.Fatal(r.Run(":8080"))
+}
