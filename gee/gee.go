7593385661c9238393da64f7f3f0fc407adf5c41
SUNLIFAN <1656680420@qq.com>
2022-07-20 16:19:20 +0800

    day 1 framework setup

--- gee/gee.go
+++ gee/gee.go
@@ -1,1 +1,36 @@
 package gee
+
+import (
+	"net/http"
+)
+
+type HandlerFunc func(w http.ResponseWriter, req *http.Request)
+
+type Engine struct {
+	route map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{route: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.route[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
