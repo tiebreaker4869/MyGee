c4a57c0adb296af08a3f58d2392eee021d33efce
SUNLIFAN <1656680420@qq.com>
2022-07-22 22:48:39 +0800

    trie.go finished

--- gee/trie.go
+++ gee/trie.go
@@ -1,5 +1,7 @@
 package gee
 
+import "strings"
+
 type node struct {
 	pattern string
 
@@ -10,10 +12,71 @@ type node struct {
 	isWild bool
 }
 
-func (*node) insert(pattern string, parts []string, height int) {
+func (n *node) getFirstMatchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
 
+	return nil
 }
 
-func (*node) search(parts []string, height int) *node {
+func (n *node) getAllMatchChildren(part string) []*node {
+	children := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			children = append(children, child)
+		}
+	}
+
+	return children
+}
+
+//parts[height] 是要插入的下一个
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+
+	child := n.getFirstMatchChild(part)
+	if child == nil {
+		child = &node{
+			pattern:  "",
+			part:     part,
+			children: make([]*node, 0),
+			isWild:   part[0] == ':' || part[0] == '*',
+		}
+		n.children = append(n.children, child)
+	}
+
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	//匹配到最后一个 part 或者匹配到第一个 * 就结束匹配
+	if height == len(parts) || strings.HasPrefix(n.part, "*") {
+		if n.part == "" {
+			return nil
+		}
+
+		return n
+	}
+
+	part := parts[height]
+
+	children := n.getAllMatchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+
+		if result != nil {
+			return result
+		}
+	}
+
 	return nil
 }
