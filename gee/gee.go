53b91f5b253f2ba6a24ee65c12a95c35828be5bf
SUNLIFAN <1656680420@qq.com>
2022-07-27 21:53:16 +0800

    router group finished

--- gee/gee.go
+++ gee/gee.go
@@ -1,29 +1,62 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix string
+
+	parent *RouterGroup
+
+	middlewares []HandlerFunc // 添加在这个路由分组上的中间件
+
+	engine *Engine // 所有的路由分组都共享一个 engine 实例
+}
+
+// 在 group 下面加一个 newGroup, 增量前缀是 prefix, 返回创建的新 group
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+
+	newGroup := &RouterGroup{
+		engine: engine,
+		prefix: group.prefix + prefix,
+		parent: group,
+	}
+
+	engine.groups = append(engine.groups, newGroup)
+
+	return newGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, compact string, handler HandlerFunc) {
+	pattern := group.prefix + compact
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
